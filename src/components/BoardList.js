68c88fdaaf3859d9c0ddebb7565bbd875a9cedb5
Tim Empey <timothyempey@gmail.com>
2022-11-18 14:02:06 -0800

    refactor file

--- src/components/BoardList.js
+++ src/components/BoardList.js
@@ -1,47 +1,55 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import Board from "./Boards";
 
 const mainBoardList = [
     {
         terrain: "All Mountain",
         price: "$600",
-        selection: [
-            "Good Company Camber",
-            "Custom X Flying V",
-            "Flight Attendant Split Board"
-        ]
+        // selection: [
+        //     "Good Company Camber",
+        //     "Custom X Flying V",
+        //     "Flight Attendant Split Board"
+        // ]
     },
     {
         terrain: "Terrain Park",
         price: "$450",
-        selection: [
-            "Blossom Camber",
-            "Name Dropper",
-            "Process Camber"
-        ]
+        // selection: [
+        //     "Blossom Camber",
+        //     "Name Dropper",
+        //     "Process Camber"
+        // ]
     },
     {
         terrain: "Powder",
         price: "$400",
-        selection: [
-            "Tree Hometown Powder",
-            "Tree 3D Daily",
-            "Family Pow Wrench"
-        ]
+        // selection: [
+        //     "Tree Hometown Powder",
+        //     "Tree 3D Daily",
+        //     "Family Pow Wrench"
+        // ]
     }
 ];
 
-function AvailableBoards() {
+function AvailableBoards(props) {
     return (
-        <div>   
-            {mainBoardList.map(terrain => {
-                return (
-                <div>
-                    <h2>{terrain.terrain} - {terrain.price}</h2>      
-                </div>
-            )
-        })}
-    </div>
- ) }
+        <React.Fragment>
+            {props.mainBoardList.map((board) => 
+            <Board
+            whenBoardClicked={props.onBoardSelection}
+            terrain = {board.terrain}
+            price = {board.price}
+            id = {board.id}
+            key = {board.id}/>
+            )}
+        </React.Fragment>
+    );
+}
+
+mainBoardList.propTypes = {
+    mainBoardList: PropTypes.array,
+    onBoardSelection: propTypes.func
+};
 
 export default AvailableBoards;
